d7c4fd4cf9985ec8b4c6633b9336b1a759d4512c
Nextzp <next_zp@vip.sina.com>
2015-09-18 16:31:16 +0800

    添加工程

---
+++ FC/Model/Timeline.h
@@ -0,0 +1,21 @@
+//
+//  Timeline.h
+//  FC
+//
+//  Created by ZhangPeng on 15/9/17.
+//  Copyright © 2015年 Lishan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Timeline : NSObject
+
+@property (strong, nonatomic) NSNumber *timelineId;
+@property (strong, nonatomic) NSString *content;
+@property (strong, nonatomic) NSNumber *createTime;
+@property (strong, nonatomic) NSString *pics;
+@property (strong, nonatomic) NSString *userIconUrl;
+@property (strong, nonatomic) NSNumber *userId;
+@property (strong, nonatomic) NSString *userNickName;
+
+@end
---
+++ FC/Model/Timeline.m
@@ -0,0 +1,14 @@
+//
+//  Timeline.m
+//  FC
+//
+//  Created by ZhangPeng on 15/9/17.
+//  Copyright © 2015年 Lishan. All rights reserved.
+//
+
+#import "Timeline.h"
+
+@implementation Timeline
+
+
+@end
